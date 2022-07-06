df6440f2ffe27fc8c78c7bb293c20318db0d117b
Jaideep Sharma <84230985+Jaideep2002@users.noreply.github.com>
2022-07-06 13:26:03 +0530

    Add files via upload

---
+++ BackEnd_Admin_Change_Password/ChangePasswordController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Rules\MatchOldPassword;
+use Illuminate\Support\Facades\Hash;
+
+class ChangePasswordController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('front.changePassword');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', new MatchOldPassword],
+            'new_password' => ['required'],
+            'new_confirm_password' => ['same:new_password'],
+        ]);
+   
+        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+        return redirect('/home/login/dashboard');
+        // dd('password updated');
+    }
+}
