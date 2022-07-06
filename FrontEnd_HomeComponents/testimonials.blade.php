3cce9fb8fae834dfbba28e8d4beb0e7a5ada9ebc
Shaik Abdul Shaan <98581805+shaikabdulshaan@users.noreply.github.com>
2022-07-06 12:27:19 +0530

    Add files via upload
    Frontend home components

---
+++ FrontEnd_HomeComponents/testimonials.blade.php
@@ -0,0 +1,105 @@
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://md-aqil.github.io/images/swiper.min.css">
+  <link rel="stylesheet" href="\dist\css\testimon.css">
+  <title>Zuccedd | Testimonials</title>
+  
+  <link rel="shortcut icon" type="image/png" href="\dist\images\header\favicon.png" />
+    <!-- Google Analytics -->
+	<link rel="shortcut icon" type="image/png" href="\dist\images\header\favicon.png" />
+<script src='../../../../google_analytics_auto.js'></script></head>
+    
+<section class="spacer">
+      
+        <div class="testimonial-section">
+          <div class="testi-user-img">
+          <div class="swiper-container gallery-thumbs">
+              <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <img class="u3" src="https://www.timexpress.ae/wp-content/uploads/2016/11/circle-man.png" alt="">
+                  </div>
+              <div class="swiper-slide">
+                <img class="u1" src="https://www.morganstanley.com/content/dam/msdotcom/articles/women-listicle/debashree-banerjee-circle.jpg" alt="">
+              </div>
+              <div class="swiper-slide">
+              <img class="u2" src="\dist\images\content\styleman.jpg" alt="">
+              </div>
+            
+              <div class="swiper-slide">
+              <img class="u4" src="\dist\images\content\djman.jpg" alt="">
+              </div>
+              
+              </div>
+            </div>
+          </div>
+          <div class="user-saying">
+              <div class="swiper-container testimonial">
+                  <!-- Additional required wrapper -->
+                  <div class="swiper-wrapper ">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                      <div class="quote">
+                          <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+                        <p>
+                        "Men's Style" is indispensable...a valuable source of practical advice for how to dress in a world of conflicting fashion."
+                        </p>
+                        <div class="name">-John Marcus-</div>
+                        <div class="designation">Bangalore</div>
+                        
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <div class="quote">
+                        <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+                      
+                        <p>
+                            “This is best and biggest unified platform
+                        for instant online admission. We can easily
+                        take admission for any course in any institute..“
+                        </p>
+                        <div class="name">-Ramkishor Verma-</div>
+                        <div class="designation">University Student</div>
+                        
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <div class="quote">
+                        <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+                          
+                        <p>
+                        The ones who constantly look out for Limited Editions and increase
+their stockpiles aren't the only ones that are called SneakerHeads.
+Some of them do it for styling and finding new colorways. They are enthusiastic about the prospect of having nicer kicks. You can become one by looking at our website, where all styling meets
+                        </p>
+                        <div class="name">David Mathew</div>
+                        <div class="designation">Mumbai</div>
+                        
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="quote">
+                          <img class="quote-icon" src="https://md-aqil.github.io/images/quote.png" alt="">
+                         
+                          <p>
+                              “This is best and biggest unified platform
+                          for instant online admission. We can easily
+                          take admission for any course in any institute..“
+                          </p>
+                          <div class="name">-Ramkishor Verma-</div>
+                          <div class="designation">University Student</div>
+                          
+                        </div>
+                      </div>
+                    
+                  </div>
+                  <!-- If we need pagination -->
+                  <div class="swiper-pagination swiper-pagination-white"></div>
+                
+                </div>
+          </div>
+        </div>
+      </section>
+  <script src="https://md-aqil.github.io/images/swiper.min.js"></script>
+  <script src="\dist\js\testmon.js"></script>
